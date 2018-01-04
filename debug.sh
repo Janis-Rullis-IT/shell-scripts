29410e101de2e96b2561e56363fa8ad521052ab9
Janis Rullis <janis.rullis.it@gmail.com>
2018-01-04 19:00:45 +0200

    Add more examples of `if` conditions and how to debug

---
+++ debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "
+=== How to debug? ===
+"
+
+echo "In the top of the script add 'set -x'. This will display comparisions 
+and other things the script is doing while executing.";
+
+echo "More info:
+* http://linuxcommand.org/lc3_wss0090.php
+";
--- if-conditions.sh
+++ if-conditions.sh
@@ -11,35 +11,59 @@ else
 fi
 ';
 
+non_existing_script='i-totaly-do-not-exist.sh';
+
+echo "Simple, boolean check to understand the structure:";
+
+echo "";
 echo "if true:";
-if [ true = true ]; then
-	echo '[ true = true ]';
+if [[ true = true ]]; then
+	echo '[[ true = true ]]';
 fi
 
 echo "";
 echo "else if true != false:";
-if [ true != true ]; then
-	echo 'if [  true != true ];';
+if [[ true != true ]]; then
+	echo 'if [  true != true ]];';
 elif [[ true != false ]]; then
 	echo 'elif [[ true != false ]];';
 fi
 
 echo "";
 echo "else:";
-if [ true != true ]; then
-	echo 'if [  true != true ];';
+if [[ true != true ]]; then
+	echo 'if [[  true != true ]];';
 else
 	echo 'else';
 fi
 
+
+echo "";
+echo "File checks:
+-d FILE	FILE exists and is a directory.
+-e FILE	FILE exists.
+-r FILE	FILE exists and the read permission is granted.
+-s FILE	FILE exists and it's size is greater than zero (ie. it is not empty).
+-w FILE	FILE exists and the write permission is granted.
+-x FILE	FILE exists and the execute permission is granted.
+";
+
 echo "";
 echo "Is this script readable?:";
-if [ -r $0 ]; then
-	echo "if [ -r $0 ]";
+if [[ -r $0 ]]; then
+	echo 'if [[ -r $0 ]]';
 fi
+
+echo "";
+echo "Is non-existing script '$non_existing_script' NOT readable?:";
+if [[ ! -r $non_existing_script ]]; then
+	echo "if [[ ! -r $non_existing_script ]];";
+fi
+
 
 echo "
 More info:
 * https://linuxacademy.com/blog/linux/conditions-in-bash-scripting-if-statements/
 * https://ryanstutorials.net/bash-scripting-tutorial/bash-if-statements.php
+* https://stackoverflow.com/a/13618376
 ";
