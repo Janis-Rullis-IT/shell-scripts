eed7237f2e44312e4771285cd907cf1d0f1b48b6
Janis Rullis <janis.rullis.it@gmail.com>
2018-01-04 19:47:10 +0200

    Add examples how to set default values if params are not passed

--- pass-arguments.sh
+++ pass-arguments.sh
@@ -7,6 +7,9 @@ values if not passed.
 This is a simple example of arguments handled based on their sequence and 
 not by parameter name like '-p' or '-u' ===";
 
+first_param='default_value_for_the_1st';
+second_param='default_value_for_the_2nd';
+
 echo "";
 echo 'This script $0 is called:';
 echo $0;
@@ -15,6 +18,7 @@ echo "";
 echo 'The first parameter $1: ';
 if [[ -n $1 ]]; then
 	echo $1;
+	first_param=$1;
 else
 		echo "Hasn't been passed. Try './pass-arguments.sh hello'";
 fi
@@ -23,6 +27,7 @@ echo "";
 echo 'The second parameter $2: ';
 if [[ -n $2 ]]; then
 	echo $2;
+	second_param=$2;
 else
 		echo "Hasn't been passed. Try './pass-arguments.sh hello there'";
 fi
@@ -34,5 +39,9 @@ echo $@;
 echo "";
 echo "Passed parameter count:";
 echo $#;
+
+echo "";
+echo "First and the second parameter. Filled if not passed:";
+echo $first_param $second_param;
 
 echo "";
