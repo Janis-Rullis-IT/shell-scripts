7c43f1a81de89c55fc8d548ba60d7e60d22dd8d3
Janis Rullis <janis.rullis.it@gmail.com>
2018-01-05 08:30:02 +0200

    Move  from the  repo

---
+++ md-tree-gen/README.md
@@ -0,0 +1,8 @@
+# /var/www/shell-scripts/md-tree-gen
+
+
+ * [md-tree-gen.sh](./md-tree-gen.sh)
+ * [README.md](./README.md)
+ * [docs](./docs)
+     * [how-to](./docs/how-to)
+         * [serve.md](./docs/how-to/serve.md)
---
+++ md-tree-gen/md-tree-gen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')
+
+tree=${tree#?}
+printf "# $PWD\n\n${tree}" > $PWD/README.md
+
