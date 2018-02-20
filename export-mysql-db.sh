cf2800eb4ee39341eaa453bb3a8b1b63589d3859
Janis Rullis <janis.rullis.it@gmail.com>
2018-02-20 19:38:44 +0200

    Add export-mysql-db.sh

---
+++ export-mysql-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Export and archive a  MYSQL database.
+
+readonly DATABASE=$1;
+readonly DATE_TIME=`date '+%Y%m%d%H%M%S'`;	
+readonly TARGET=$DATE_TIME-$DATABASE.sql
+
+mysqldump -uroot -p --default-character-set=utf8 $DATABASE > $TARGET;
+7z a $TARGET.7z $TARGET;
