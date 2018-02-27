8477af9aaacb8924db7eccdbadd87b1b74abf1ad
Janis Rullis <janis.rullis.it@gmail.com>
2018-02-27 11:59:46 +0200

    Add gitp - Quickly push changes You're sure about

---
+++ gitp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Make globally available with:
+# sudo cp gitp.sh /usr/local/bin/gitp
+# sudo chmod a+x /usr/local/bin/gitp
+
+echo "== gitp - Quickly push changes You're sure about."
+echo "== Short for 'git add .;git commit -m "MSG";git push' ==";
+echo "";
+
+if [[ -n $1 ]]; then
+	readonly MSG=$1;
+	`git add .`;
+	`git commit -m "$MSG"`
+else
+	echo "Commit message is missing!";
+	echo "Example, gitp 'Update README'";
+fi
