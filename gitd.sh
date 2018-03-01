493f8851f6afbdf1b60bc9dfbda624a7014db46f
Janis Rullis <janis.rullis.it@gmail.com>
2018-03-01 13:16:05 +0200

    Add shortcuts for git - gits and gitd

---
+++ gitd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Make globally available with:
+# sudo cp gitd.sh /usr/local/bin/gitd
+# sudo chmod a+x /usr/local/bin/gitd
+
+echo "git diff";
+git diff
---
+++ gits.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Make globally available with:
+# sudo cp gits.sh /usr/local/bin/gits
+# sudo chmod a+x /usr/local/bin/gits
+
+echo "git status";
+git status
