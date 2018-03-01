caeb6860cc5ac7a49bf1066009363f4b560c1ac1
Janis Rullis <janis.rullis.it@gmail.com>
2018-03-01 13:03:32 +0200

    Add mdpr.sh - preview md from terminal using ReText

---
+++ mdpr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Preview Markdown files from terminal using ReText's preview mode.
+# Make globally avaolable
+# sudo mdpr.sh  /usr/local/bin/mdpr
+# sudo chmod a+x  /usr/local/bin/mdpr
+
+retext --preview $1
+
