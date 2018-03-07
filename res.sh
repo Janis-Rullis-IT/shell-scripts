fbe8e69e0434a4f44ab74cb1c69acb95e5bb5545
Janis Rullis <janis.rullis.it@gmail.com>
2018-03-07 09:59:08 +0200

    Add instruct. how to make res globally available

--- res.sh
+++ res.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## Make globally available with:
+# sudo cp res.sh /usr/local/bin/res
+# sudo chmod a+x /usr/local/bin/res
+
 echo "== Resize images with ImageMagick ==";
 
 convert "*.png[250x>]" -set filename:base "%[base]" "%[filename:base]-250x.png"
