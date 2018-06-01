a213e2fe3f58eb318ec2de811fff7fc11645077d
Janis Rullis <janis.rullis.it@gmail.com>
2018-06-01 10:39:55 +0300

    Fn: Check if func perm. affect cd

--- learn-basics/func.sh
+++ learn-basics/func.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Example of shell functions with arguments.
+# Also check if directory is permanently changed in a function.
+#
 # https://bash.cyberciti.biz/guide/Pass_arguments_into_a_function
 # https://stackoverflow.com/a/6212408
 
 echo "== Hi func ==
 ";
+
+echo "Current directory: ${PWD}";
 
 function hi ()
 {
@@ -12,6 +16,14 @@ function hi ()
 	name=$2
 
 	echo "Hi ${text}${name}";
+	echo "I'll move 1 directory up.";
+	cd ..;
+	echo "Current directory: ${PWD}"; 
+	
 }
 
 hi  'there, ' $1
+echo "After function current directory: ${PWD}
+"; 
+
+echo "== Conclusion: Functions affect permamently current directory. =="
