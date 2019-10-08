7ea741f47ceb50273182f756aa25ecd8976e460c
Janis Rullis <janis.rullis.it@gmail.com>
2019-10-08 21:17:45 +0300

    Add iwg.sh - images for web - rename, EXIF, JPG

---
+++ iwg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## Make globally available with:
+# sudo cp iwg.sh /usr/local/bin/iwg
+# sudo chmod a+x /usr/local/bin/iwg
+
+echo "== Prepare images for web - rename, add EXIF, convert to sRGB JPG ==
+Example
+iwg \"Flowers\" \"\-by-Janis-Rullis\" \"2019:09:30\" \"21:00:00\"
+";
+
+# Copy me, please!
+# iwg "Flowers" "-by-Janis-Rullis" "2019:09:30" "21:00:00"
+
+#set -Eeuo pipefail; # set -o xtrace;
+# Define how to handle newlines in the `find` results.
+IFS=$'\n\t'
+
+DESCRIPTION=$1
+END=$2
+DATE=$3
+TIME=$4
+
+ren "${DESCRIPTION}" "${END}"
+cd renamed
+
+# Add converted sRPG JPGs.
+srg
+cd srgb
+
+# Set EXIF for originals.
+exi "${DESCRIPTION}" "${DATE}" "${TIME}"
+
+echo "Final result is in \"${PWD}/renamed/srgb\""
