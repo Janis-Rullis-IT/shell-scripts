c8d28f5b88a9b9f841202255f0ac04f7f1e1f135
Janis Rullis <janis.rullis.it@gmail.com>
2020-04-02 18:44:20 +0300

    Update vid.sh

--- vid.sh
+++ vid.sh
@@ -5,9 +5,14 @@
 # sudo chmod a+x /usr/local/bin/vid
 
 echo "== Create a video from images with a music ==";
+size=1920:1080;
+
+if [[ -n $1 ]]; then
+        size=$1;
+fi
 
 # https://video.stackexchange.com/a/23532
-ffmpeg -framerate 1/10 -i img%03d.jpg -r 100 -pix_fmt yuv420p out.mp4
+ffmpeg -framerate 1/10 -i img-%04d.jpg -r 100 -pix_fmt yuv420p out.mp4
 
 # https://www.youtube.com/watch?v=lE1y_TTISTQ
-ffmpeg -i out.mp4 -i audio.mp3 -c copy -shortest out_with_audio.mp4 
+ffmpeg -i out.mp4 -i /usr/local/bin/audio.mp3 -c copy -shortest out_with_audio.mp4
