f50eccf0404858b68219597cf8c57236ffb693f6
Janis Rullis <janis.rullis.it@gmail.com>
2020-05-03 15:28:27 +0300

    #9 Create mag.sh - Collect files with matching filenames and any extension

---
+++ mag.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# sudo ln -s ~/Desktop/www/shell-scripts/mag.sh /usr/local/bin/mag && sudo chmod a+x /usr/local/bin/mag
+
+echo "== Collect files with matching filenames and any extension. ==";
+
+if [[ ! -n $1 ]]; then
+        echo "From which directory should collect files?";
+        exit;
+fi
+
+DIR_FROM=$1;
+DIR_TO=$(basename -- "$DIR_FROM")
+
+if [[ ! -d ${DIR_TO} ]]; then
+        mkdir ${DIR_TO};
+        echo "Created ${DIR_TO}";
+fi
+
+for f in  `find . -maxdepth 1 -type f` 
+do
+        # #9 Format the filename without the extension https://stackoverflow.com/a/965072
+        filename=$(basename -- "$f")
+        search="${filename%.*}";
+
+        # #9 Look for files in the desired directory.
+        for f2 in  `find ${DIR_FROM} -type f -name "${search}.*"` 
+        do
+          echo $f2;
+          cp "${f2}" "${DIR_TO}/"
+        done        
+done
