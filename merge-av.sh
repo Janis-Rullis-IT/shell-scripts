39d1755a63ecb804b6154b38403d07130cc182d7
Janis Rullis <janis.rullis.it@gmail.com>
2021-08-21 22:19:12 +0300

    Update merge-av.sh

--- merge-av.sh
+++ merge-av.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # https://superuser.com/a/277667
-# ffmpeg -i FILENAME.MOV -i FILENAME.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 FILENAME-1.MOV
+# ffmpeg -i FILENAME.MOV -i FILENAME.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 -shortest -y FILENAME-1.MOV
